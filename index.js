58f0dccee5cf481dd8c09769121872d8c4e79f63
Spycrab-db <gavinyan2004@gmail.com>
2023-05-19 01:39:11 -0400

    Deleted unecessary function for the console game

--- index.js
+++ index.js
@@ -22,31 +22,6 @@ function playRound(playerSelection, computerSelection) {
     return 'lose';
 }
 
-function game() {
-    let playerScore = computerScore = 0;
-    while (playerScore < 5 && computerScore < 5) {
-        let playerChoice = parseInt(prompt("Choose your play: 0-Rock, 1-Paper, 2-Scissors"));
-        let computerChoice = getRandChoice();
-        result = playRound(playerChoice, computerChoice);
-
-        const select = ["Rock", "Paper", "Scissors"];
-        if (result === 'win') {
-            alert(`You win! ${select[playerChoice]} beats ${select[computerChoice]}`);
-            playerScore++;
-        }
-        else if (result === 'lose') {
-            alert(`You lose! ${select[computerChoice]} beats ${select[playerChoice]}`);
-            computerScore++;
-        }
-        else {
-            alert("It's a tie!");
-        }
-        alert(`You: ${playerScore}\nComputer: ${computerScore}`);
-    }
-    if (playerScore === 5) alert("You won best of 5!");
-    else alert("Oh no! You lost best of 5.");
-}
-
 //Selecting all relavant elements
 let rock = document.querySelector('#rock');
 let paper = document.querySelector('#paper');
